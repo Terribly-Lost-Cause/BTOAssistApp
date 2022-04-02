e17cac660f491334963653a9341cf8f8a3f0af39
changzuhe <changzuhe4@gmail.com>
2022-04-03 01:38:17 +0800

    add page

---
+++ BTOAssistApp/BTOAssistApp/Views/ApplicationPage3.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BTOAssistApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ApplicationPage3 : ContentPage
+    {
+        public ApplicationPage3()
+        {
+            InitializeComponent();
+        }
+    }
+}
