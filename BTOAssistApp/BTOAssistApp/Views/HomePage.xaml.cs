ec16d89be2432ff4f6b43e3f056a216ef881a160
Terribly-Lost-Cause <limcavell9@gmail.com>
2022-05-17 19:57:33 +0800

    reeee

--- BTOAssistApp/BTOAssistApp/Views/HomePage.xaml.cs
+++ BTOAssistApp/BTOAssistApp/Views/HomePage.xaml.cs
@@ -25,9 +25,67 @@ namespace BTOAssistApp.Views
         private string id;
         private string accesstoken;
         private string warningmsg;
+        private string firstcarosellvisibility;
+        private string secondcarosellvisibility;
+        private string pickerresultvisibility;
         private ObservableCollection<BTO> allbto;
         public ObservableCollection<BTO> btosorted;
-        HttpClient client = new HttpClient();
+        public ObservableCollection<BTO> btofilter;
+        private ObservableCollection<string> locationitems;
+        private ObservableCollection<string> roomtypeitems;
+
+    HttpClient client = new HttpClient();
+
+        public string FirstCarouselVisibility
+        {
+            get { return firstcarosellvisibility; }
+            set
+            {
+                firstcarosellvisibility = value;
+                OnPropertyChanged(nameof(FirstCarouselVisibility)); // Notify that there was a change on this property
+            }
+        }
+
+        public string SecondCarouselVisibility
+        {
+            get { return secondcarosellvisibility; }
+            set
+            {
+                secondcarosellvisibility = value;
+                OnPropertyChanged(nameof(SecondCarouselVisibility)); // Notify that there was a change on this property
+            }
+        }
+
+        public string PickerResultVisibility
+        {
+            get { return pickerresultvisibility; }
+            set
+            {
+                pickerresultvisibility = value;
+                OnPropertyChanged(nameof(PickerResultVisibility)); // Notify that there was a change on this property
+            }
+        }
+
+        public ObservableCollection<string> LocationItems
+        {
+            get { return locationitems; }
+            set
+            {
+                locationitems = value;
+                OnPropertyChanged(nameof(LocationItems)); // Notify that there was a change on this property
+            }
+        }
+
+        public ObservableCollection<string> RoomtypeItems
+        {
+            get { return roomtypeitems; }
+            set
+            {
+                roomtypeitems = value;
+                OnPropertyChanged(nameof(RoomtypeItems)); // Notify that there was a change on this property
+            }
+        }
+
         public string WarningMsg
         {
             get { return warningmsg; }
@@ -84,11 +142,21 @@ namespace BTOAssistApp.Views
                 OnPropertyChanged(nameof(BTOSorted)); // Notify that there was a change on this property
             }
         }
+
+        public ObservableCollection<BTO> BTOFilter
+        {
+            get { return btofilter; }
+            set
+            {
+                btofilter = value;
+                OnPropertyChanged(nameof(BTOFilter)); // Notify that there was a change on this property
+            }
+        }
         public HomePage()
         {
             base.OnAppearing();
             WarningMsg = "False";
-
+            
             GetLink();
             CallGetBTOLink();
             InitializeComponent();
@@ -99,9 +167,14 @@ namespace BTOAssistApp.Views
 
         protected override async void OnAppearing()
         {
+            FirstCarouselVisibility = "true";
+            SecondCarouselVisibility = "true";
+            PickerResultVisibility = "false";
             WarningMsg = "False";
-
+            GetPickerItems();
             GetLink();
+
+
         }
         protected async void GetLink()
         {
@@ -177,25 +250,185 @@ namespace BTOAssistApp.Views
 
         }
 
+        protected async void GetPickerItems()
+        {
+            Uri getBTOInfo = new Uri("https://uwuwuwuwuuwuwuwuwuuwuwuwuwuuwu.herokuapp.com/pickerItems");
+            HttpResponseMessage response = await client.GetAsync(getBTOInfo);
+            string content = await response.Content.ReadAsStringAsync();
+            JObject data = JObject.Parse(content);
+            var locations = data["location"];
+            var rooms = data["room"];
+            ObservableCollection<string> TempLocation = new ObservableCollection<string>();
+            ObservableCollection<string> TempRoom = new ObservableCollection<string>();
+            if(LocationItems == null){
+                Console.WriteLine("It's null");
+            }
+            TempLocation.Add("All Locations");
+            TempRoom.Add("All Room Types");
 
-        /*
-                protected override void OnDisappearing()
-                {
-                    base.OnDisappearing();
+            foreach (var x in locations)
+            {
+                /*string name = x.Key;
+                JToken value = x.Value;*/
+                Console.WriteLine(x["location"].ToString());
+                TempLocation.Add( x["location"].ToString());
+                
+                //
+            }
 
-                    ObservableCollection<BTO> TempAllBTO = new ObservableCollection<BTO>();
-                    ObservableCollection<BTO> TempBTOSorted = new ObservableCollection<BTO>();
-                    AllBTO = TempAllBTO;
-                    BTOSorted = TempBTOSorted;
-                    Console.WriteLine(TempBTOSorted.Count());
-                    BindingContext = this;
-                }
-        */
+            foreach (var x in rooms)
+            {
+                /*string name = x.Key;
+                JToken value = x.Value;*/
+                TempRoom.Add(x["roomtype"].ToString() + " Rooms");
+            }
+
+            LocationItems = TempLocation;
+            RoomtypeItems = TempRoom;
+            BindingContext = this;
+
+
+        }
+
         async void ViewDetail(object sender, EventArgs args)
         {
             var BTO = (Frame)sender;
             string id = BTO.AutomationId;
             await Navigation.PushAsync(new BTODetail(id));
+        }
+        //getLocationPickerItems
+
+        
+        public async void Location_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+            string selectedIndex = "";
+            //picker.SelectedItem = null;
+            Console.WriteLine("Location: "+Location.SelectedIndex);
+            Console.WriteLine("Picker: "+picker.SelectedIndex.ToString());
+            if (picker.SelectedIndex == -1)
+            {
+
+                return;
+            }
+            else { 
+                selectedIndex = picker.SelectedItem.ToString();
+                const string filterLocation = "https://uwuwuwuwuuwuwuwuwuuwuwuwuwuuwu.herokuapp.com/getLocationPickerItems";
+                Uri newGetUrl;
+
+                if (selectedIndex != "All Locations")
+                {
+                    var selectedLocation = new Dictionary<string, string>
+                      {
+                          { "location", selectedIndex.ToString()}
+
+                      };
+                    newGetUrl = new Uri(QueryHelpers.AddQueryString(filterLocation, selectedLocation));
+                }
+                else
+                {
+                    Console.WriteLine("It's everything");
+                    var selectedLocation = new Dictionary<string, string>
+                      {
+                          { "location", "everything"}
+
+                      };
+                    newGetUrl = new Uri(QueryHelpers.AddQueryString(filterLocation, selectedLocation));
+                }
+
+
+                    HttpResponseMessage response = await client.GetAsync(newGetUrl);
+                    string content = await response.Content.ReadAsStringAsync();
+                    JObject data = JObject.Parse(content);
+                    var filterarray = data["getLocation"] as JArray;
+                    ObservableCollection<BTO> TempBTOFilter = new ObservableCollection<BTO>();
+
+                    foreach (var filterhouse in filterarray)
+                    {
+                        var filterclasshouse = filterhouse.ToObject<BTO>();
+                        filterclasshouse.Block = "Block " + filterclasshouse.Block;
+                        TempBTOFilter.Add(filterclasshouse);
+
+
+
+                    }
+                    BTOFilter = TempBTOFilter;
+                    Console.WriteLine(TempBTOFilter.Count());
+                    BindingContext = this;
+
+
+                    FirstCarouselVisibility = "false";
+                    SecondCarouselVisibility = "false";
+                    PickerResultVisibility = "true";
+                    BindingContext = this;
+            }
+        }
+
+        private async void Room_Type_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+            string selectedIndex = "";
+            //picker.SelectedItem = null;
+            Console.WriteLine("Location: " + Location.SelectedIndex);
+            Console.WriteLine("Picker: " + picker.SelectedIndex.ToString());
+            if (picker.SelectedIndex == -1)
+            {
+
+                return;
+            }
+            else
+            {
+                selectedIndex = picker.SelectedItem.ToString();
+                const string filterLocation = "https://uwuwuwuwuuwuwuwuwuuwuwuwuwuuwu.herokuapp.com/getRoomPickerItems";
+                Uri newGetUrl;
+
+                if (selectedIndex != "All Room Types")
+                {
+                    var noOfRooms = selectedIndex.ToString().Substring(0, 1);
+                    Console.WriteLine(noOfRooms.ToString());
+                    var selectedLocation = new Dictionary<string, string>
+                      {
+                          { "room", noOfRooms.ToString()}
+
+                      };
+                    newGetUrl = new Uri(QueryHelpers.AddQueryString(filterLocation, selectedLocation));
+                }
+                else
+                {
+                    var selectedLocation = new Dictionary<string, string>
+                      {
+                          { "room", "everything"}
+
+                      };
+                    newGetUrl = new Uri(QueryHelpers.AddQueryString(filterLocation, selectedLocation));
+                }
+
+
+                    HttpResponseMessage response = await client.GetAsync(newGetUrl);
+                    string content = await response.Content.ReadAsStringAsync();
+                    JObject data = JObject.Parse(content);
+                    var filterarray = data["getRooms"] as JArray;
+                    ObservableCollection<BTO> TempBTOFilter = new ObservableCollection<BTO>();
+
+                    foreach (var filterhouse in filterarray)
+                    {
+                        var filterclasshouse = filterhouse.ToObject<BTO>();
+                        filterclasshouse.Block = "Block " + filterclasshouse.Block;
+                        TempBTOFilter.Add(filterclasshouse);
+
+
+
+                    }
+                    BTOFilter = TempBTOFilter;
+                    Console.WriteLine(TempBTOFilter.Count());
+                    BindingContext = this;
+
+
+                    FirstCarouselVisibility = "false";
+                    SecondCarouselVisibility = "false";
+                    PickerResultVisibility = "true";
+                    BindingContext = this;
+            }
         }
     }
 }
